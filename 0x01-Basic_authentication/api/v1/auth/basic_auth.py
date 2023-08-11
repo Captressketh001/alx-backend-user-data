859909feacaed020b232e8b5872af17aba2bd1c3
Oluwakemi Omoyeni <shininglite2012@gmail.com>
2023-08-11 03:39:20 +0100

    created basic authentication class

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,5 +1,7 @@
+#!/usr/bin/env python3
+"""Basic Auth"""
 from api.v1.auth.auth import Auth
 
 
 class BasicAuth(Auth):
-    """ a class BasicAuth that inherits from Auth"""
+    """a class BasicAuth that inherits from Auth"""
