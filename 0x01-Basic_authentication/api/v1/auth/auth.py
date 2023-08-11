7e081a005e35464328f8a027a9e734b784ade4af
Oluwakemi Omoyeni <shininglite2012@gmail.com>
2023-08-11 03:12:52 +0100

    update

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """ Module of Index views
 """
+import re
 from flask import request
 from typing import List, TypeVar
 
@@ -9,10 +10,23 @@ class Auth:
     """a class to manage the API authentication"""
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Require Auth"""
-        return False
+        if path is not None and excluded_paths is not None:
+            for ex in map(lambda x: x.strip(), excluded_paths):
+                pattern = ''
+                if ex[-1] == '*':
+                    pattern = '{}.*'.format(ex[0:-1])
+                elif ex[-1] == '/':
+                    pattern = '{}/*'.format(ex[0:-1])
+                else:
+                    pattern = '{}.*'.format(ex)
+                if re.match(pattern, path):
+                    return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """Authorization"""
+        if request is not None:
+            return request.headers.get('Authorization', None)
         return None
 
     def current_user(self, request=None) -> TypeVar('User'):
