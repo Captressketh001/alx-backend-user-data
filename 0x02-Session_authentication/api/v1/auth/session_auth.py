9203b2bd51c08ccb74ad17db4fcbacdfebce5ded
Oluwakemi Omoyeni <shininglite2012@gmail.com>
2023-08-12 22:55:43 +0100

    session class

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,7 +1,17 @@
 #!/usr/bin/env python3
 """Session Auth"""
+
+from uuid import uuid4
 from .auth import Auth
 
 
 class SessionAuth(Auth):
     """a class SessionAuth that inherits from Auth"""
+    user_id_by_session_id = dict()
+
+    def create_session(self, user_id: str = None) -> str:
+        """Create a session id"""
+        if isinstance(user_id, str):
+            session_id = str(uuid4())
+            self.user_id_by_session_id[session_id] = user_id
+            return session_id
